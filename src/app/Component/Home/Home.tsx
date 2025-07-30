7d5f68dc269c8ab6efa5f49f1d7ab9762f75bfe3
pradeep <relatablepradeep@gmail.com>
2025-07-30 22:40:58 +0530

    added home section

--- src/app/Component/Home/Home.tsx
+++ src/app/Component/Home/Home.tsx
@@ -1,9 +1,31 @@
-export default function Home(){
+export default function MYhome(){
 
     return(
         <>
 
-        
+        <main>
+
+            <section className="flex border-2 w-full h-min-screen" >
+
+                <div className="flex border-2 w-full h-min-screen">
+
+                    hiii
+
+
+
+                </div>
+
+                <div  className="  w-full h-min-screen">
+                    hello
+
+
+                </div>
+
+            </section>
+
+        </main>
+
+
         
         
         
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Nav from "./Component/Header/Nav";
+import MYhome from "./Component/Home/Home";
+
 
 export default function Home() {
   return (
    <div>
 
     <Nav/>
+
+
+    <MYhome/>
 
    </div>
   );
