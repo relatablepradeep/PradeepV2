08f33f7a9ff87e03a0e758254112db9480a20137
pradeep <relatablepradeep@gmail.com>
2025-08-30 19:33:27 +0530

    finally home page respsoive ness complete

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -23,26 +23,26 @@ export default function MyHome() {
 
   return (
     <main className="fixed left-0 w-1/2 h-screen hidden md:block bg-amber-50">
-      <div className="flex p-3  ">
-        <div className="relative inline-block">
+      <div className="flex p-6  2xl:p-14  ">
+        <div className="relative inline-block  ">
           <Image
             src={Photo}
             width={150}
             height={150}
             alt="author"
-            className="rounded-full shadow-lg  lg:h-52 lg:w-52"
+            className="rounded-full shadow-lg  lg:h-52 lg:w-52  xl:h-64 xl:w-64   2xl:w-72 2xl:h-72"
           />
 
           {/* Flag on top of photo */}
           <div className="absolute    z-20">
-            <Flag code="IN" className="md:w-12 md:h-8   lg:w-16 lg:h-12 lg:bottom-52 lg:rounded rounded shadow-lg relative md:bottom-37" />
+            <Flag code="IN" className="md:w-12 md:h-8   lg:w-16 lg:h-12 lg:bottom-52   xl:h-16 xl:w-20   2xl:w-20 2xl:h-24 xl:bottom-64  2xl:bottom-76 lg:rounded rounded shadow-lg relative md:bottom-37" />
           </div>
         </div>
 
-        <div className="md:text-2xl text-black    relative   md:top-16 md:left-6">
+        <div className="md:text-2xl text-black   lg:text-4xl lg:top-20 2xl:text-6xl xl:text-5xl   2xl:top-30 xl:top-28  2xl:left-12 xl:left-12   relative   md:top-16 md:left-6">
 
           <h2 className="  ">Pradeep!</h2>
-           <div className="md:w-32 md:h-28 overflow-hidden text-sm text-black">
+           <div className="md:w-32 md:h-28 lg:text-3xl overflow-hidden md:text-sm text-black">
             <Typing />
           </div>
         </div>
